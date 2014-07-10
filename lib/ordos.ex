6588db56da3a2c2b1c17c140e7366e121cf6c16d
d0rc <7993863@gmail.com>
2014-07-10 15:13:44 +0400

    starting one node

--- lib/ordos.ex
+++ lib/ordos.ex
@@ -7,8 +7,9 @@ defmodule Ordos do
     children = [
       # Define workers and child supervisors to be supervised
       # worker(Ordos.Worker, [arg1, arg2, arg3])
+      worker(:locker, [1])
     ]
-
+    :locker.set_nodes([:erlang.node | :erlang.nodes], [:erlang.node], :erlang.nodes)
 
     opts = [strategy: :one_for_one, name: Ordos.Supervisor]
     Supervisor.start_link(children, opts)
